9ff5a649a8182bda774eadf7231583e834da561b
Mauricio Jost <mauricio.jost@gmail.com>
2015-10-29 22:36:30 +0100

    Fix size of sample input file

--- localhdfs/scripts/generate-sample-file.sh
+++ localhdfs/scripts/generate-sample-file.sh
@@ -4,20 +4,22 @@ function r() {
     echo $RANDOM,xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 }
 
-echo Step 1
+
+echo "### Generating a file of 24G..."
+echo "# Step 1"
 for i in `seq 1 40000`
 do
   r >> gen1.csv
 done
 
-echo Step 2
-for i in `seq 1 200`
+echo "# Step 2"
+for i in `seq 1 250`
 do
   cat gen1.csv >> gen2.csv
 done
 
-echo Step 3
-for i in `seq 1 2`
+echo "# Step 3"
+for i in `seq 1 4`
 do
   cat gen2.csv >> big.csv
 done
@@ -25,4 +27,4 @@ done
 rm gen*.csv
 mv big.csv ../input/
 
-echo Done
+echo "### Done"
