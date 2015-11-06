f3001a5d06501f77cb84ab97bd4bf1624c2622df
Mauricio Jost <mauricio.jost@gmail.com>
2015-11-06 20:40:37 +0100

    Provide initialize.sh

---
+++ initialize.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CLASS=eu.pepot.eu.examples.Example
+JAR=./target/scala-2.10/spark-example_2.10-0.1.jar
+
+if [ -f localhdfs/input/big.csv ] 
+then
+  echo "### Input file already existent. Skipping generation..."
+else
+  echo "### Generating input file..."
+  mkdir -p localhdfs/input
+  ./localhdfs/scripts/generate-sample-file.sh
+fi
+
+echo "### Generating artifacts..."
+sbt package 
+
+echo "### Ready to run (see conf directory first):"
+echo "       spark-submit --class $CLASS $JAR"
+
