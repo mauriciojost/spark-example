81df3b9285b9adae82974a89871003edafe8a88d
Mauricio Jost <mauricio.jost@gmail.com>
2015-11-15 08:59:38 +0100

    Remove lessons from code
    They are in the Google Doc now.

--- src/main/scala/eu/pepot/eu/examples/Example.scala
+++ src/main/scala/eu/pepot/eu/examples/Example.scala
@@ -3,23 +3,6 @@ package eu.pepot.eu.examples
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
-// LESSONS:
-// - Use array types that might end up being primitive light-weight JVM types
-// - Avoid conversions as much as possible
-// - Avoid groupBy! Use better reduceByKey (to discard elements as soon as possible, decreases execution time by half!!!)
-// - Think about repartitioning to avoid overhead of data transfer when having too many partitions on distant executors (reduces 1/7 the time)
-// - Think better about coalescing with the amount of workers, it can only decrease the amount of partitions with less data transfering than a full shuffle
-//   and speds up processing (decreases execution time to one third of the original one!!!!)
-// - Coalescing (4 threads in local master):
-//    NO  ->  47s
-//     4  ->  26s
-//     8  ->  26s
-//    16  ->  28s
-//    32  ->  31s
-//    64  ->  38s
-//   128  ->  46s
-
-
 object Example {
 
   type CsvLine = Array[String]
