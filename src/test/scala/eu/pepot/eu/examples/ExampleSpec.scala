7a25d66dbdca7ba434c3af5419a3b6cc5e72a8ae
Mauricio Jost <mauricio.jost@gmail.com>
2015-11-15 12:20:53 +0100

    Add a test

---
+++ src/test/scala/eu/pepot/eu/examples/ExampleSpec.scala
@@ -0,0 +1,28 @@
+package eu.pepot.eu.examples
+
+import com.holdenkarau.spark.testing.{RDDComparisions, SharedSparkContext}
+import eu.pepot.eu.examples.Helper.{Event, FlaggedEvent}
+import org.scalatest.FunSuite
+
+class ExampleSpec extends FunSuite with SharedSparkContext {
+
+  test("simple flagging") {
+    val events = List(
+      Event(1999, 0, ""),
+      Event(1999, 1, ""),
+      Event(2000, 2, ""),
+      Event(2000, 3, "")
+    )
+    val expected = List(
+      FlaggedEvent(1999, 0, false, ""),
+      FlaggedEvent(1999, 1, true, ""),
+      FlaggedEvent(2000, 2, false, ""),
+      FlaggedEvent(2000, 3, true, "")
+    )
+
+    assert(None ===
+      RDDComparisions.compare(sc.parallelize(expected), Flagger.flagEventsRDD(sc.parallelize(events))(sc)))
+  }
+
+}
+
