20b7a2e97f1f20f2fe96eb2a249d16721c258e9c
Mauricio Jost <mauricio.jost@gmail.com>
2015-11-15 08:42:31 +0100

    Put logs somewhere else

--- initialize.bash
+++ initialize.bash
@@ -2,10 +2,12 @@
 
 export PATH=$PATH:$HOME/opt/spark/bin/
 mkdir -p /tmp/spark-events
+mkdir -p logs
 
 CLASS=eu.pepot.eu.examples.Example
 JAR=./target/scala-2.10/spark-example_2.10-0.1.jar
-LOG=execution.log
+LOG=logs/output.log
+TOOK_LOG=logs/took.log
 
 if [ -f data/input/big.csv ] 
 then
@@ -26,6 +28,7 @@ export CURRENT_COMMIT=`git rev-parse HEAD`
 export SPARK_CONF_DIR=`pwd`/conf/
 spark-submit --class $CLASS $JAR &> $LOG
 echo $CURRENT_COMMIT >> $LOG
+cat $LOG | grep took > $TOOK_LOG
 
 echo "### Done."
 
